6aca70bb1d4be0e9cc651b90a894f371465f50db
Dmitry <dmsobolev@yahoo.com>
2020-08-22 23:48:48 +0300

    parser update. try operator added to pass the errors

--- parser.py
+++ parser.py
@@ -10,20 +10,25 @@ driver.get('https://sketchfab.com/3d-models/categories/characters-creatures?date
 
 button_next = driver.find_elements_by_css_selector(".load-next")[0]
 # load page
-for i in range(10):
-    ActionChains(driver).move_to_element(button_next).perform()
-    button_next.click()
-    sleep(10)
+for i in range(1000):
+    try:
+        ActionChains(driver).move_to_element(button_next).perform()
+        button_next.click()
+        print(f"Iter #{i} of 1000")
+        sleep(3)
+    except:
+        break
 
 # get urls
 data = driver.find_elements_by_class_name("card-model, card")
 url_list = []
-for index, item in enumerate(data):
-    url = data[index].find_elements_by_tag_name('meta')[5].get_attribute("content")
+for item in data:
+    url = item.find_elements_by_tag_name('meta')[5].get_attribute("content")
     url_list.append(url)
 
-with open('url_list.txt', 'w') as file:
+# save result
+with open('character_and_creatures.txt.txt', 'w') as file:
     for item in url_list:
         file.writelines(item + '\n')
 
-print('Connected')
+print('Process finished')
